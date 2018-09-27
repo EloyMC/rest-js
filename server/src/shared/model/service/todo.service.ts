da5a45031c1ac6dc23ca0611e35fd6f765c55820
Eloy Maduro <eloymaduro@gmail.com>
2018-09-27 20:55:45 +0200

    build pls

--- server/src/shared/model/service/todo.service.ts
+++ server/src/shared/model/service/todo.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { ToDo } from '../entity/todo.entity';
 
-
+// todo service
 @Injectable()
 export class ToDoService {
     constructor(
